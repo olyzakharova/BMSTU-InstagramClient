5c7ca926607ddde586c4ce284436953ac259ae7b
Oly <hoit@bk.ru>
2016-10-24 22:31:50 +0300

    Update client.hpp

--- include/instagram/client.hpp
+++ include/instagram/client.hpp
@@ -1,1 +1,11 @@
+#include <string>
+#include <map>
 
+namespace Instagram{
+  class Client{
+  public:
+    using dict_t = std::map<std::string, std::string>;
+    Client(dict_t settings);
+    auto check_connection() -> bool;
+  };
+}
