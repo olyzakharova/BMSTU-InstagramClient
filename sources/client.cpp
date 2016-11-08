570e3a7198df618f838210948fa5752d72e4e157
Oly <hoit@bk.ru>
2016-11-08 23:57:04 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -36,10 +36,12 @@ namespace Instagram
     CURL *curl = curl_easy_init();
       if (curl)
       {
-                   
-        curl_easy_setopt(curl, CURLOPT_URL, "https://api.instagram.com/v1/users/self/?access_token=4079059384.39f3c6e.29a442eca7f54865a92e071db07cd987"); 
+        
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.instagram.com/v1/users/self/follows?access_token=4079059384.39f3c6e.29a442eca7f54865a92e071db07cd987"); 
         res = curl_easy_perform(curl);
         if (res == CURLE_OK)
+        {}
       }
   
   }
