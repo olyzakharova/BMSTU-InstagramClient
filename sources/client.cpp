adc235c7e0661bb763f4e36222fdddc68cd99e3f
Oly <hoit@bk.ru>
2016-11-08 23:55:34 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -28,4 +28,19 @@ namespace Instagram
       }
     
    }
+   
+  
+  
+  auto InstagramClient::get_followers ()-> bool
+  { 
+    CURL *curl = curl_easy_init();
+      if (curl)
+      {
+                   
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.instagram.com/v1/users/self/?access_token=4079059384.39f3c6e.29a442eca7f54865a92e071db07cd987"); 
+        res = curl_easy_perform(curl);
+        if (res == CURLE_OK)
+      }
+  
+  }
 }
