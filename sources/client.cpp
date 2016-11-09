8ce580fab856e257fc0cb1a6aa3603df1dcb5862
Oly <hoit@bk.ru>
2016-11-09 21:10:36 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -31,18 +31,18 @@ namespace Instagram
    
   
   
-  auto InstagramClient::get_followers ()-> void
-  { 
-    CURL *curl = curl_easy_init();
-      if (curl)
-      {
+ // auto InstagramClient::get_followers ()-> void
+ // { 
+   // CURL *curl = curl_easy_init();
+    //  if (curl)
+    //  {
         
-        CURLcode res;
-        curl_easy_setopt(curl, CURLOPT_URL, "https://api.instagram.com/v1/users/self/follows?access_token=4079059384.39f3c6e.29a442eca7f54865a92e071db07cd987"); 
-        res = curl_easy_perform(curl);
-        if (res == CURLE_OK)
-        {}
-      }
+      //  CURLcode res;
+      //  curl_easy_setopt(curl, CURLOPT_URL, "https://api.instagram.com/v1/users/self/follows?access_token=4079059384.39f3c6e.29a442eca7f54865a92e071db07cd987"); 
+      //  res = curl_easy_perform(curl);
+      //  if (res == CURLE_OK)
+     //   {}
+     // }
   
-  }
+//  }
 }
