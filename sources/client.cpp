cdc2f0b848ec4495c09cc38c5469568006d4118f
Oly <hoit@bk.ru>
2016-11-12 15:04:31 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -17,7 +17,7 @@ namespace Instagram
 
 			CURLcode res;
 			string token = "4079059384.39f3c6e.29a442eca7f54865a92e071db07cd987";
-			curl_easy_setopt(curl, CURLOPT_URL, (string("https://api.instagram.com/v1/users/self/?access_token=")+token).c_str())
+			curl_easy_setopt(curl, CURLOPT_URL, (string("https://api.instagram.com/v1/users/self/?access_token=")+token).c_str());
 			res = curl_easy_perform(curl);
 			if (res == CURLE_OK)
 			{
