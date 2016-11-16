acd2ffb2dd2f92d4087ceccb15860be69ca4201e
Oly <hoit@bk.ru>
2016-11-17 00:20:22 +0300

    Update client.hpp

--- include/instagram/client.hpp
+++ include/instagram/client.hpp
@@ -8,13 +8,17 @@ namespace Instagram {
 
 		 public:
 			using dict_t = std::map<std::string, std::string>;
+                         auto callback(char *ptr_, size_t size_, size_t nmemb_, string &userdata_);
 
 			InstagramClient() {};
 			InstagramClient(dict_t settings) : settings_(settings) {};
 			auto check_connection() -> bool;
+                        auto get_most_recent_media()->void;
+                      //  auto liked_by_user()-> void;
 		
     private:
 			dict_t settings_;
+                        //auto callback(char *ptr_, size_t size_, size_t nmemb_, string &userdata_);
 
 	};
 }
