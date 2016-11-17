75dbdca0a6be2caccc0f61b089ea96a5d05872e4
Oly <hoit@bk.ru>
2016-11-17 23:32:55 +0300

    add exceptions :)

--- sources/client.cpp
+++ sources/client.cpp
@@ -13,33 +13,34 @@ namespace Instagram
 
     using json = nlohmann::json;
 
-
-    static string buffer;
-
     size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp)
        {
+                 string* buffer = (string*)userp;
                  size_t realsize = size * nmemb;
-                 buffer.append((char*)contents, realsize);
-                    return realsize;
+                 buffer->append((char*)contents, realsize);
+                 return realsize;
        }
 
     auto InstagramClient::check_connection()-> bool
         {
 
                  CURL *curl = curl_easy_init();
+                 string json_;
+                 
                  if (curl)
         {
 
             CURLcode res;
             string token = "4079059384.39f3c6e.29a442eca7f54865a92e071db07cd987";
             curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+            curl_easy_setopt(curl,CURLOPT_WRITEDATA, &json_);
             curl_easy_setopt(curl, CURLOPT_URL, (string("https://api.instagram.com/v1/users/self/?access_token=")+token).c_str());
-            buffer.clear();
             res = curl_easy_perform(curl);
-            if (res == CURLE_OK)
+			
+            if (res == CURLE_OK && json_ != "")
                 {
                     boost::property_tree::ptree pt;
-                    stringstream ss(buffer);
+                    stringstream ss(json_);
                     boost::property_tree::read_json(ss,pt);
                     boost::property_tree::write_json(cout, pt);
                     cout << "authentication is successful" << endl;
@@ -51,6 +52,7 @@ namespace Instagram
                     cout << "your access token isn't correct" << endl;
                     return false;
                 }
+          
             curl_easy_cleanup(curl);
         }
 
@@ -62,19 +64,26 @@ namespace Instagram
     auto InstagramClient::get_most_recent_media()->string
     {
 		CURL *curl = curl_easy_init();
-        string json_;
+		string json_;
 		if (curl)
 		{
 			CURLcode res;
 			curl_easy_setopt(curl, CURLOPT_URL, "https://api.instagram.com/v1/users/self/media/recent/?access_token=4079059384.39f3c6e.29a442eca7f54865a92e071db07cd987");
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,write_callback);
-            curl_easy_setopt(curl,CURLOPT_WRITEDATA, &json_);
-            res = curl_easy_perform(curl);
-            if (res == CURL_OK && json != "")
+                        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,write_callback);
+			curl_easy_setopt(curl,CURLOPT_WRITEDATA, &json_);
+                        res = curl_easy_perform(curl);
+			
+                        if (res == CURLE_OK && json_ != "")
             {
-                json link =(json::parse(json_))["data"]["link"];
-                curl_easy_cleanup(curl);
-                return link;
+				json link =(json::parse(json_))["data"][0]["link"];
+				
+                                if(link.is_null())
+                                   throw std::runtime_error("no link member");
+				
+                                string str = link.dump(4);
+				cout << "link: " << str << endl;
+                                curl_easy_cleanup(curl);
+				return str;
 
             }
 
