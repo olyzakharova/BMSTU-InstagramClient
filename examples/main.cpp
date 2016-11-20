5b158a478b018a256e8e9fd0033c1ea89af72b3a
Oly <hoit@bk.ru>
2016-11-20 21:51:28 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,12 +1,24 @@
 #include "instagram/client.hpp"
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
-  Instagram::InstagramClient client;
-  client.check_connection();
-  client.get_most_recent_media();
-  //client.liked_by_user();
+  try
+  {
+    Instagram::InstagramClient client;
+    client.check_connection();
+    client.get_most_recent_media();
+  
+  }
+  catch(exception& e)
+  {
+    cout << "exception: " << e.what() << std::endl;
+  }
 
 
   return 0;
 }
+
